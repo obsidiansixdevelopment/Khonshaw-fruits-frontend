76e63a5bbc1db93f35b8b08b1246c7a92e31464b
Aniketkalawat <94599673+aniketkalawat88@users.noreply.github.com>
2025-01-09 22:05:41 +0530

    Update whatsapp.jsx

--- src/app/_components/whatsapp.jsx
+++ src/app/_components/whatsapp.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default function Whatsapp() {
   return (
     <div>
-        <Link href="https://api.whatsapp.com/send?phone=+9647509252524&text=Hey!%20Can%20I%20Get%20More%20Info%20On%20This?" target='_blank' className="fixed text-4xl right-[3vw] bottom-[3vw] z-30 max-sm:p-4">
+        <Link href="https://api.whatsapp.com/send?phone=+971504459495&text=Hey!%20Can%20I%20Get%20More%20Info%20On%20This?" target='_blank' className="fixed text-4xl right-[3vw] bottom-[3vw] z-30 max-sm:p-4">
             <div className="grid justify-items-center gap-2">
                 <div className="contact_icon ">
                     <img src="/image.png" alt="" className=" my-float h-12 w-12" />
